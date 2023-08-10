d42d320e83bb8aa64694757d8d3f2ab3096f7bbc
joseph heath <heathj873@gmail.com>
2023-08-10 17:11:01 -0400

    first

---
+++ CoffeeShopProductLab/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using CoffeeShopProductLab.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+
+namespace CoffeeShopProductLab.Controllers
+{
+    public class ProductController : Controller
+    {
+        private CoffeeProductsDbContext _coffeeProductsDbContext = new CoffeeProductsDbContext();
+        public IActionResult Index()
+        {
+            List<Product> result = _coffeeProductsDbContext.Products.ToList();
+            return View(result);
+        }
+        public IActionResult Details(int id)
+        {
+            Product p = _coffeeProductsDbContext.Products.FirstOrDefault(x => x.Id == id);
+            
+            return View(p);
+        }
+    }
+}
